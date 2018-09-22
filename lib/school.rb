af1b75973a2c3806462acd2969f30782ff1120ff
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-22 02:03:37 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School
+  
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] = [] if @roster[grade] == nil
+    @roster[grade] << student
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.collect{|grade, students| grade[students].sort}
+  end
+end
